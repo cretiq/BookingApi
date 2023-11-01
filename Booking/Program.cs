8a87e802d5ca10b75a9d2914972382a9bef70ffa
filipmellqvist <filip_mellqvist@msn.com>
2023-11-01 15:13:44 +0100

    Reapply db migrate

--- Booking/Program.cs
+++ Booking/Program.cs
@@ -18,10 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(connectionString
 builder.Services.AddIdentityCore<MyUser>().AddEntityFrameworkStores<AppDbContext>().AddApiEndpoints();
 builder.Services.AddControllers();
 
-Console.WriteLine("THIS IS CONNECTION STRING!!!");
-Console.WriteLine(connectionString);
-
-// builder.Services.BuildServiceProvider().GetService<AppDbContext>().Database.Migrate();
+builder.Services.BuildServiceProvider().GetService<AppDbContext>().Database.Migrate();
 
 var app = builder.Build();
 
